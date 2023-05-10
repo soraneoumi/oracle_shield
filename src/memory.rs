218d67e40a2a7ed2b80cdd0f5ac67a49c9d5fc74
soraneoumi <22672990+soraneoumi@users.noreply.github.com>
2023-05-10 18:11:28 +0900

    Add lock_guard

--- src/memory.rs
+++ src/memory.rs
@@ -1,6 +1,7 @@
 use rand::Rng;
 use std::sync::{Arc, Mutex};
 use std::thread;
+use std::time::Duration;
 
 const KIB: usize = 1024;
 const MIB: usize = 1024 * KIB;
@@ -13,17 +14,31 @@ pub struct GiBObject {
 pub fn memory(gib: i32) -> Vec<GiBObject> {
     let mut buffers = Vec::with_capacity(gib as usize);
     let mut threads = Vec::new();
+
     for _ in 0..gib {
         let v = Arc::new(Mutex::new(vec![0; GIB]));
         let o = GiBObject { b: v.clone() };
         buffers.push(o);
+
         let handle = thread::spawn(move || {
             let mut rng = rand::thread_rng();
-            let mut v = v.lock().unwrap();
-            for byte in &mut *v {
-                *byte = rng.gen::<u8>();
+
+            loop {
+                match v.try_lock() {
+                    Ok(mut v_guard) => {
+                        for byte in &mut *v_guard {
+                            *byte = rng.gen::<u8>();
+                        }
+                        break;
+                    }
+                    Err(_) => {
+                        // Sleep for 10ms and try again
+                        thread::sleep(Duration::from_millis(10));
+                    }
+                }
             }
         });
+
         threads.push(handle);
     }
 
