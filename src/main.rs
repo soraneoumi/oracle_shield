6b02cef12c2dbb6ceb54d5a1e6eb472b4fddc288
soraneoumi <22672990+soraneoumi@users.noreply.github.com>
2023-05-12 00:32:47 +0900

    main.rs modularization

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,15 @@
 mod memory;
+mod args;
 
-use std::env;
-use std::process;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
 use std::thread;
 use std::time::Duration;
+use args::parse_args;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() != 3 {
-        eprintln!("Usage: {} -m <memory in GiB>", args[0]);
-        process::exit(1);
-    }
-    let memory_arg = match args[2].parse::<i32>() {
-        Ok(memory) => memory,
-        Err(err) => {
-            eprintln!("Error parsing memory argument: {}", err);
-            process::exit(1);
-        }
-    };
-    let _buffers = memory::memory(memory_arg);
+    let args = parse_args();
+    let _buffers = memory::memory(args.memory);
 
     // Keep the program running until Ctrl-C is pressed
     let running = Arc::new(AtomicBool::new(true));
